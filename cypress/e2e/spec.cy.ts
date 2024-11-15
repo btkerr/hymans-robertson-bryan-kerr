6f996bf407eba84f4a6d963f7d708adc3c498230
Bryan Kerr <btkerr13@gmail.com>
2024-11-15 09:59:46 +0000

    cy spec

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,8 @@
-describe("template spec", () => {
+describe("basic template spec", () => {
   it("passes", () => {
-    cy.visit("https://example.cypress.io");
+    cy.visit(
+      "https://main--673712a83fc20ae35be08554.chromatic.com/iframe.html?globals=&args=&id=technical-test-dashboard--user-interface&viewMode=story"
+    );
+    cy.get("h1").should("contain.text", "Welcome");
   });
 });
